e09747d7bfc11d40f4299d7de34121a385453108
Andrew <andrewryanogle@gmail.com>
2020-02-24 18:25:49 -0800

    finished mvp

--- adv.py
+++ adv.py
@@ -28,6 +28,45 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+reverese_directions = {'n':'s', 's':'n','e':'w','w':'e'}
+
+reverse_path = []
+
+visited = {}
+
+
+#start with current room and get it exits
+visited[player.current_room.id] = player.current_room.get_exits()
+
+# travers the graph while the rooms visited are less than graph
+while len(visited) < len(room_graph) -1:
+	#if the current room is not in visited 
+	if player.current_room.id not in visited:
+		 # add it
+		visited[player.current_room.id] = player.current_room.get_exits()
+		# remove the exit one at a time so we dont traverse the same direction.
+		visited[player.current_room.id].remove(reverse_path[-1]) 
+	
+	
+    # while the room has no more exits 
+	while len(visited[player.current_room.id]) == 0:
+		# get the last direction w/ pop()
+		reverse = reverse_path.pop()
+		# update traversal Path
+		traversal_path.append(reverse)
+		#update player movement in the opisite direction 
+		player.travel(reverse)
+	
+	#go to first available exit
+	movement = visited[player.current_room.id].pop()
+
+	#update the path 
+	traversal_path.append(movement)
+	#update the reverse path 
+	reverse_path.append(reverese_directions[movement])
+	#update player movement
+	player.travel(movement)
+
 
 
 
@@ -37,14 +76,14 @@ player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
 
 for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
+	player.travel(move)
+	visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+	print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
-    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+	print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+	print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
 
@@ -53,10 +92,10 @@ else:
 #######
 player.current_room.print_room_description(player)
 while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+	cmds = input("-> ").lower().split(" ")
+	if cmds[0] in ["n", "s", "e", "w"]:
+		player.travel(cmds[0], True)
+	elif cmds[0] == "q":
+		break
+	else:
+		print("I did not understand that command.")
